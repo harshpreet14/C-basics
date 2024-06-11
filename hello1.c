58b6f16b578cabcecad2281010d7e8b4e509737a
Harshpreet Kaur <114172812+harshpreet14@users.noreply.github.com>
2024-06-11 18:59:59 +0530

    Update hello1.c

--- hello1.c
+++ hello1.c
@@ -1,1 +1,17 @@
+#include<stdio.h>
+int add(a, b){
+  return a+b ;
+}
 
+int main(void) {
+  int x,y;
+
+  printf("Enter any two numbers: ");
+  scanf("%d %d", &x, &y);
+  printf("%d\n%d\n%d\n", x, y, add(x+y)); 
+  
+
+  
+  return 0;
+  
+}
